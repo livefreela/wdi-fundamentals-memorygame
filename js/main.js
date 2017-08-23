56ad3b236c39b733389290b06a8cc764f7b25a5d
livefreela <michaelbrawer@me.com>
2017-08-23 02:01:56 -0700

    created cardId func

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardOne === cardTwo) {
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   }
   else {
     alert("Sorry, try again.");
   }
-}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId){
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+flipCard(0);
+flipCard(1);
