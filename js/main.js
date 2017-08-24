da1c7477f243d9857fbbc230410db03cfc82a9ca
livefreela <michaelbrawer@me.com>
2017-08-24 04:48:42 -0700

    added score counter & reset logic

--- js/main.js
+++ js/main.js
@@ -23,26 +23,9 @@ var cards = [
 ];
 
 var cardsInPlay = [];
-
-var checkForMatch = function(){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again.");
-  }
-};
-
-var flipCard = function(){
-  var cardId = this.getAttribute('data-id');
-  console.log("User flipped " + cards[cardId].rank);
-  cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  this.setAttribute('src', cards[cardId].cardImage);
-  if (cardsInPlay.length === 2) {
-    checkForMatch();
-  }
-};
+var messageDisplay = document.querySelector('#message');
+var score = 0;
+var tallyUp = document.querySelector('#tally');
 
 var createBoard = function(){
   for (var i = 0; i < cards.length; i++){
@@ -50,8 +33,71 @@ var createBoard = function(){
     cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute("data-id", i);
     cardElement.addEventListener('click', flipCard);
+
     document.getElementById('game-board').appendChild(cardElement);
   }
 };
 
+//return cards to default state
+var resetBoard = function() {
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.getElementsByTagName('img');
+    cardElement[i].setAttribute('src', 'images/back.png');
+    cardsInPlay = [];
+    messageDisplay.textContent = "Pick a Card";
+    shuffleArray(cards);
+  }
+};
+
+
+//shuffles the order of cards displayed
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+//flips cards, checks card count
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
+  console.log("User flipped " + cards[cardId].rank);
+  messageDisplay.textContent = 'Now Match It!';
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+
+};
+
+//checks for card match
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    // alert("You found a match!");
+    messageDisplay.textContent = 'Correct!';
+    score += 10;
+    tallyUp.textContent = score;
+    setTimeout(resetBoard, 800);
+  }
+  else {
+    // alert("Sorry, try again.");
+    messageDisplay.textContent = 'Try Again...';
+    cardsInPlay.pop();
+  }
+};
+
+var resetCount = function() {
+  score = 0;
+  tallyUp.textContent = score;
+};
+
 createBoard();
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetBoard);
+resetButton.addEventListener('click', resetCount);
